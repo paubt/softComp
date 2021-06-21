34ea081edf6f72061e9f6b23aaa6f0f19c37e217
paubt <pau_b@protonmail.com>
2021-06-21 23:38:12 +0200

    add input buttons

--- widgets/nn/perceptron.cpp
+++ widgets/nn/perceptron.cpp
@@ -5,14 +5,12 @@
 #include "perceptron.h"
 
 Perceptron::Perceptron() {
-    //Colors
-    ombra_ = new QString("QWidget { background: #f9ebef;"
-                         "              color: black }"
-                             );
-
+    //init perceptron values
+    input_1_ =  new int;   input_2_ =  new int;   input_3_ =  new int;   input_4_ =  new int;
+    weight_1_ = new float; weight_2_ = new float; weight_3_ = new float; weight_4_ = new float;
 
     perceptron_window = new QWidget();
-    perceptron_window->setStyleSheet(*ombra_);
+    perceptron_window->setStyleSheet(ombra_);
     setCentralWidget(perceptron_window);
 
     //top layout  mainly for title
@@ -34,7 +32,7 @@ Perceptron::Perceptron() {
 
     //mid layout for explanation and formulas
 
-    label_mid_explain_ = new QLabel("kek");
+    label_mid_explain_ = new QLabel("lel");
     label_mid_explain_->setFrameStyle(QFrame::Panel | QFrame::Raised);
     label_mid_explain_->setAlignment(Qt::AlignCenter);
     label_mid_explain_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
@@ -44,23 +42,147 @@ Perceptron::Perceptron() {
 
     //bottom Perceptron layout
 
-    //inputs are 4 widgets with
-    input_slider_1_ = new QSlider;
-    input_slider_1_->setTickInterval(1);
-    input_slider_1_->setSingleStep(1);
+    //inputs
+    {
+        input_label_title_ = new QLabel("Inputs");
+        input_label_title_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+        input_label_title_->setAlignment(Qt::AlignCenter);
+        title_font->setPointSize(13);
+        input_label_title_->setFont(*title_font);
 
-    input_label_1_ = new QLabel("test");
+        input_button_1_on_ = new QPushButton("1");
+        setAutoFillBackground(input_button_1_on_);
+        QObject::connect(input_button_1_on_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_1_on_clicked);
+        input_button_1_off_ = new QPushButton("0");
+        QObject::connect(input_button_1_off_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_1_off_clicked);
+        input_layout_1_ = new QHBoxLayout();
+        input_layout_1_->addWidget(input_button_1_on_);
+        input_layout_1_->addWidget(input_button_1_off_);
 
-    QObject::connect(input_slider_1_, &QAbstractSlider::valueChanged,
-                     this, &Perceptron::input_slider_1_changed);
+        input_button_2_on_ = new QPushButton("1");
+        setAutoFillBackground(input_button_2_on_);
+        QObject::connect(input_button_2_on_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_2_on_clicked);
+        input_button_2_off_ = new QPushButton("0");
+        QObject::connect(input_button_2_off_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_2_off_clicked);
+        input_layout_2_ = new QHBoxLayout();
+        input_layout_2_->addWidget(input_button_2_on_);
+        input_layout_2_->addWidget(input_button_2_off_);
 
-    v_layout_bot_inputs_ = new QVBoxLayout;
-    v_layout_bot_inputs_->addWidget(input_slider_1_);
-    v_layout_bot_inputs_->addWidget(input_label_1_);
+        input_button_3_on_ = new QPushButton("1");
+        setAutoFillBackground(input_button_3_on_);
+        QObject::connect(input_button_3_on_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_3_on_clicked);
+        input_button_3_off_ = new QPushButton("0");
+        QObject::connect(input_button_3_off_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_3_off_clicked);
+        input_layout_3_ = new QHBoxLayout();
+        input_layout_3_->addWidget(input_button_3_on_);
+        input_layout_3_->addWidget(input_button_3_off_);
+
+        input_button_4_on_ = new QPushButton("1");
+        setAutoFillBackground(input_button_4_on_);
+        QObject::connect(input_button_4_on_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_4_on_clicked);
+        input_button_4_off_ = new QPushButton("0");
+        QObject::connect(input_button_4_off_, &QPushButton::clicked,
+                         this, &Perceptron::input_button_4_off_clicked);
+        input_layout_4_ = new QHBoxLayout();
+        input_layout_4_->addWidget(input_button_4_on_);
+        input_layout_4_->addWidget(input_button_4_off_);
+
+        v_layout_bot_inputs_ = new QVBoxLayout;
+        v_layout_bot_inputs_->addWidget(input_label_title_);
+        v_layout_bot_inputs_->addLayout(input_layout_1_);
+        v_layout_bot_inputs_->addLayout(input_layout_2_);
+        v_layout_bot_inputs_->addLayout(input_layout_3_);
+        v_layout_bot_inputs_->addLayout(input_layout_4_);
+
+
+    }
+
+    //weights
+    {
+        int maxSlider = 1000000;
+
+        weight_label_title_ = new QLabel("Weights");
+        weight_label_title_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+        weight_label_title_->setAlignment(Qt::AlignCenter);
+        weight_label_title_->setFont(*title_font);
+        //inputs are 4 widgets with
+        weight_slider_1_ = new QSlider(Qt::Orientation::Horizontal);
+        weight_slider_1_->setMaximum(maxSlider);
+        weight_slider_1_->setTickInterval(1);
+        weight_label_1_ = new QLabel("0");
+        QObject::connect(weight_slider_1_, &QAbstractSlider::valueChanged,
+                         this, &Perceptron::weight_slider_1_changed);
+        weight_layout_1_ = new QHBoxLayout;
+        weight_layout_1_->addWidget(weight_slider_1_);
+        weight_layout_1_->addWidget(weight_label_1_);
+
+        weight_slider_2_ = new QSlider(Qt::Orientation::Horizontal);
+        weight_slider_2_->setMaximum(maxSlider);
+        weight_slider_2_->setTickInterval(1);
+        weight_label_2_ = new QLabel("0");
+        QObject::connect(weight_slider_2_, &QAbstractSlider::valueChanged,
+                         this, &Perceptron::weight_slider_2_changed);
+        weight_layout_2_ = new QHBoxLayout;
+        weight_layout_2_->addWidget(weight_slider_2_);
+        weight_layout_2_->addWidget(weight_label_2_);
+
+        weight_slider_3_ = new QSlider(Qt::Orientation::Horizontal);
+        weight_slider_3_->setMaximum(maxSlider);
+        weight_slider_3_->setTickInterval(1);
+        weight_label_3_ = new QLabel("0");
+        QObject::connect(weight_slider_3_, &QAbstractSlider::valueChanged,
+                         this, &Perceptron::weight_slider_3_changed);
+        weight_layout_3_ = new QHBoxLayout;
+        weight_layout_3_->addWidget(weight_slider_3_);
+        weight_layout_3_->addWidget(weight_label_3_);
+
+        weight_slider_4_ = new QSlider(Qt::Orientation::Horizontal);
+        weight_slider_4_->setMaximum(maxSlider);
+        weight_slider_4_->setTickInterval(1);
+        weight_label_4_ = new QLabel("0");
+        QObject::connect(weight_slider_4_, &QAbstractSlider::valueChanged,
+                         this, &Perceptron::weight_slider_4_changed);
+        weight_layout_4_ = new QHBoxLayout;
+        weight_layout_4_->addWidget(weight_slider_4_);
+        weight_layout_4_->addWidget(weight_label_4_);
+
+        //layout
+        v_layout_bot_weights_ = new QVBoxLayout;
+        v_layout_bot_weights_->addWidget(weight_label_title_);
+        v_layout_bot_weights_->addLayout(weight_layout_1_);
+        v_layout_bot_weights_->addLayout(weight_layout_2_);
+        v_layout_bot_weights_->addLayout(weight_layout_3_);
+        v_layout_bot_weights_->addLayout(weight_layout_4_);
+
+    }
+
+    //internal
+    {
+        internal_label_title = new QLabel("Internal");
+        internal_label_title->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+        internal_label_title->setAlignment(Qt::AlignCenter);
+        internal_label_title->setFont(*title_font);
+
+        internal_label_fa_ = new QLabel(QString::fromUtf8("fa() = \u2211 input"));
+
+        v_layout_bot_internal_ = new QVBoxLayout;
+        v_layout_bot_internal_->addWidget(internal_label_title);
+        v_layout_bot_internal_->addWidget(internal_label_fa_);
+    }
+
 
 
     h_layout_bot_ = new QHBoxLayout;
     h_layout_bot_->addLayout(v_layout_bot_inputs_);
+    h_layout_bot_->addLayout(v_layout_bot_weights_);
+    h_layout_bot_->addLayout(v_layout_bot_internal_);
 
     auto *layout = new QVBoxLayout();
     layout->setContentsMargins(5,5,5,5);
@@ -71,6 +193,85 @@ Perceptron::Perceptron() {
     perceptron_window->setLayout(layout);
 }
 
-void Perceptron::input_slider_1_changed(int value) {
-    input_label_1_->setNum(value);
+void Perceptron::weight_slider_1_changed(int value) {
+    *weight_1_ = value/1000000.0;
+    std::string temp = std::to_string(*weight_1_);
+    weight_label_1_->setText(QString::fromStdString(temp));
 }
+void Perceptron::weight_slider_2_changed(int value) {
+    *weight_2_ = value/1000000.0;
+    std::string temp = std::to_string(*weight_2_);
+    weight_label_2_->setText(QString::fromStdString(temp));
+}
+void Perceptron::weight_slider_3_changed(int value) {
+    *weight_3_ = value/1000000.0;
+    std::string temp = std::to_string(*weight_3_);
+    weight_label_3_->setText(QString::fromStdString(temp));
+}
+void Perceptron::weight_slider_4_changed(int value) {
+    *weight_4_ = value/1000000.0;
+    std::string temp = std::to_string(*weight_4_);
+    weight_label_4_->setText(QString::fromStdString(temp));
+}
+
+void Perceptron::input_button_1_on_clicked() {
+    //set internal var
+    *input_1_ = 1;
+    //set button color for visual feedback
+    input_button_1_on_->setStyleSheet(input_button_active_);
+    input_button_1_off_->setStyleSheet(input_button_passive_);
+
+}
+void Perceptron::input_button_1_off_clicked() {
+    //set internal var
+    *input_1_ = 0;
+    //set button color for visual feedback
+    input_button_1_on_->setStyleSheet(input_button_passive_);
+    input_button_1_off_->setStyleSheet(input_button_active_);
+}
+void Perceptron::input_button_2_on_clicked() {
+    //set internal var
+    *input_2_ = 1;
+    //set button color for visual feedback
+    input_button_2_on_->setStyleSheet(input_button_active_);
+    input_button_2_off_->setStyleSheet(input_button_passive_);
+}
+void Perceptron::input_button_2_off_clicked() {
+    //set internal var
+    *input_2_ = 0;
+    //set button color for visual feedback
+    input_button_2_on_->setStyleSheet(input_button_passive_);
+    input_button_2_off_->setStyleSheet(input_button_active_);
+}
+void Perceptron::input_button_3_on_clicked() {
+    //set internal var
+    *input_3_ = 1;
+    //set button color for visual feedback
+    input_button_3_on_->setStyleSheet(input_button_active_);
+    input_button_3_off_->setStyleSheet(input_button_passive_);
+}
+void Perceptron::input_button_3_off_clicked() {
+    //set internal var
+    *input_3_ = 0;
+    //set button color for visual feedback
+    input_button_3_on_->setStyleSheet(input_button_passive_);
+    input_button_3_off_->setStyleSheet(input_button_active_);
+}
+void Perceptron::input_button_4_on_clicked() {
+    //set internal var
+    *input_4_ = 1;
+    //set button color for visual feedback
+    input_button_4_on_->setStyleSheet(input_button_active_);
+    input_button_4_off_->setStyleSheet(input_button_passive_);
+}
+void Perceptron::input_button_4_off_clicked() {
+    //set internal var
+    *input_4_ = 0;
+    //set button color for visual feedback
+    input_button_4_on_->setStyleSheet(input_button_passive_);
+    input_button_4_off_->setStyleSheet(input_button_active_);
+}
+
+
+
+
--- widgets/nn/perceptron.h
+++ widgets/nn/perceptron.h
@@ -5,12 +5,18 @@
 #ifndef SOFTCOMP_PERCEPTRON_H
 #define SOFTCOMP_PERCEPTRON_H
 
+#include "string"
+
 #include "QMainWindow"
 #include "QLabel"
 #include "QVBoxLayout"
 #include "QHBoxLayout"
 #include "QFont"
 #include "QSlider"
+#include "QPushButton"
+#include "QPalette"
+
+class InputSlider;
 
 class Perceptron: public QMainWindow {
     Q_OBJECT
@@ -19,9 +25,29 @@ public:
     QWidget *perceptron_window;
 
 public slots:
-    void input_slider_1_changed(int value);
+    //inputs
+    void input_button_1_on_clicked();
+    void input_button_1_off_clicked();
+    void input_button_2_on_clicked();
+    void input_button_2_off_clicked();
+    void input_button_3_on_clicked();
+    void input_button_3_off_clicked();
+    void input_button_4_on_clicked();
+    void input_button_4_off_clicked();
+
+    //weights
+    void weight_slider_1_changed(int value);
+    void weight_slider_2_changed(int value);
+    void weight_slider_3_changed(int value);
+    void weight_slider_4_changed(int value);
 private:
-    QString *ombra_;
+    int   *input_1_,  *input_2_,  *input_3_,  *input_4_;
+    float *weight_1_, *weight_2_, *weight_3_, *weight_4_;
+    //colors
+    const QString input_button_active_  = QString("background-color: green");
+    const QString input_button_passive_ = QString("background-color: red");
+    const QString ombra_= QString("QWidget { background: #f9ebef;"
+                                      "          color: black }");;
 
     //top layout title
     QLabel *label_title_;
@@ -32,16 +58,57 @@ private:
     QLabel *label_mid_explain_;
 
     //bot layout for the Perceptron visualization
-    QHBoxLayout *h_layout_bot_;
+    //inputs
+    QLabel *input_label_title_;
+
+    QPushButton *input_button_1_on_;
+    QPushButton *input_button_1_off_;
+    QHBoxLayout *input_layout_1_;
+
+    QPushButton *input_button_2_on_;
+    QPushButton *input_button_2_off_;
+    QHBoxLayout *input_layout_2_;
+
+    QPushButton *input_button_3_on_;
+    QPushButton *input_button_3_off_;
+    QHBoxLayout *input_layout_3_;
+
+    QPushButton *input_button_4_on_;
+    QPushButton *input_button_4_off_;
+    QHBoxLayout *input_layout_4_;
+
     QVBoxLayout *v_layout_bot_inputs_;
+
+    //Weights
+    QLabel *weight_label_title_;
+    QSlider *weight_slider_1_;
+    QLabel *weight_label_1_;
+    QHBoxLayout *weight_layout_1_;
+
+    QSlider *weight_slider_2_;
+    QLabel *weight_label_2_;
+    QHBoxLayout *weight_layout_2_;
+
+    QSlider *weight_slider_3_;
+    QLabel *weight_label_3_;
+    QHBoxLayout *weight_layout_3_;
+
+    QSlider *weight_slider_4_;
+    QLabel *weight_label_4_;
+    QHBoxLayout *weight_layout_4_;
+
     QVBoxLayout *v_layout_bot_weights_;
-    QVBoxLayout *v_layout_bot_perceptron_;
+
+    //internal
+    QLabel *internal_label_title;
+    QLabel *internal_label_fa_;
+
+    QVBoxLayout *v_layout_bot_internal_;
+
+    //output
     QVBoxLayout *v_layout_bot_output;
-
-    //input layout
-    QSlider *input_slider_1_;
-    QLabel *input_label_1_;
-
+    //main layout bot
+    QHBoxLayout *h_layout_bot_;
 };
 
 #endif //SOFTCOMP_PERCEPTRON_H
